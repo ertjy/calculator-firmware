04b6441673df7b128889b6b629f85f7a55a06d52
Simonas Giniunas <simoas.giniunas1@gmailc.com>
2024-07-10 19:15:20 +0300

    Implement button driver

---
+++ src/button_driver.rs
@@ -0,0 +1,112 @@
+use alloc::boxed::Box;
+use alloc::vec;
+use alloc::vec::Vec;
+use core::convert::Infallible;
+use debouncr::{debounce_16, debounce_stateful_16, Debouncer, DebouncerStateful, Edge, Repeat16};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+
+const ROWS: usize = 9;
+const COLUMNS: usize = 3;
+
+const BUTTONS: [[ButtonClick; COLUMNS]; ROWS] = [
+    [
+        ButtonClick::Digit1,
+        ButtonClick::Digit2,
+        ButtonClick::Digit3,
+    ],
+    [
+        ButtonClick::Digit4,
+        ButtonClick::Digit5,
+        ButtonClick::Digit6,
+    ],
+    [
+        ButtonClick::Digit7,
+        ButtonClick::Digit8,
+        ButtonClick::Digit9,
+    ],
+    [
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+    ],
+    [
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+    ],
+    [
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+    ],
+    [
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+    ],
+    [
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+    ],
+    [
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+        ButtonClick::Ignored,
+    ],
+];
+
+pub struct ButtonDriver {
+    col_pins: Vec<Box<dyn OutputPin<Error = Infallible>>>,
+    row_pins: Vec<Box<dyn InputPin<Error = Infallible>>>,
+    debouncers: [[Debouncer<u16, Repeat16>; COLUMNS]; ROWS],
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum ButtonClick {
+    Ignored,
+    Digit0,
+    Digit1,
+    Digit2,
+    Digit3,
+    Digit4,
+    Digit5,
+    Digit6,
+    Digit7,
+    Digit8,
+    Digit9,
+}
+
+impl ButtonDriver {
+    pub fn new(
+        col_pins: Vec<Box<dyn OutputPin<Error = Infallible>>>,
+        row_pins: Vec<Box<dyn InputPin<Error = Infallible>>>,
+    ) -> Self {
+        let mut debouncers =
+            core::array::from_fn(|_i| core::array::from_fn(|_i| debounce_16(false)));
+        Self {
+            col_pins,
+            row_pins,
+            debouncers,
+        }
+    }
+
+    pub fn get_clicks(&mut self) -> Vec<ButtonClick> {
+        let mut clicks = vec![];
+        for col_pin_index in 0..self.col_pins.len() {
+            let col_pin = &mut self.col_pins[col_pin_index];
+
+            col_pin.set_high().unwrap();
+            for row_pin_index in 0..self.row_pins.len() {
+                let row_pin = &self.row_pins[row_pin_index];
+                if let Some(Edge::Rising) =
+                    self.debouncers[row_pin_index][col_pin_index].update(row_pin.is_high().unwrap())
+                {
+                    clicks.push(BUTTONS[row_pin_index][col_pin_index]);
+                }
+            }
+            col_pin.set_low().unwrap();
+        }
+        clicks
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+#![no_std]
+#![no_main]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
+
+use alloc::alloc::Layout;
+use alloc::boxed::Box;
+use alloc::vec;
+use alloc::vec::Vec;
+use alloc_cortex_m::CortexMHeap;
+use core::convert::Infallible;
+use panic_rtt_target as _;
+
+use calculator_firmware_library::button_driver::ButtonDriver;
+use cortex_m_rt::entry;
+use debouncr::{debounce_16, debounce_stateful_16};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use fugit::HertzU32;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f1xx_hal::device::{CorePeripherals, Peripherals};
+use stm32f1xx_hal::gpio::Input;
+use stm32f1xx_hal::prelude::*;
+
+#[global_allocator]
+pub static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+#[alloc_error_handler]
+fn oom(_: Layout) -> ! {
+    panic!("Ran out of memory");
+}
+
+pub fn allocate_heap(heap_size: usize, allocator: &'static CortexMHeap) {
+    let start = cortex_m_rt::heap_start() as usize;
+    unsafe { allocator.init(start, heap_size) }
+}
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+
+    let mut dp = Peripherals::take().unwrap();
+    let cp = CorePeripherals::take().unwrap();
+
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc
+        .cfgr
+        .use_hse(HertzU32::MHz(8))
+        .sysclk(HertzU32::MHz(72))
+        .hclk(HertzU32::MHz(72))
+        .pclk1(HertzU32::MHz(36))
+        .pclk2(HertzU32::MHz(72));
+
+    allocate_heap(4096, &ALLOCATOR);
+
+    let mut gpioc = dp.GPIOC.split();
+
+    let mut col_pins: Vec<Box<dyn OutputPin<Error = Infallible>>> = vec![
+        Box::new(gpioc.pc0.into_push_pull_output(&mut gpioc.crl)),
+        Box::new(gpioc.pc1.into_push_pull_output(&mut gpioc.crl)),
+        Box::new(gpioc.pc2.into_push_pull_output(&mut gpioc.crl)),
+    ];
+
+    let mut row_pins: Vec<Box<dyn InputPin<Error = Infallible>>> = vec![
+        Box::new(gpioc.pc3.into_pull_down_input(&mut gpioc.crl)),
+        Box::new(gpioc.pc4.into_pull_down_input(&mut gpioc.crl)),
+        Box::new(gpioc.pc5.into_pull_down_input(&mut gpioc.crl)),
+        Box::new(gpioc.pc6.into_pull_down_input(&mut gpioc.crl)),
+        Box::new(gpioc.pc7.into_pull_down_input(&mut gpioc.crl)),
+        Box::new(gpioc.pc8.into_pull_down_input(&mut gpioc.crh)),
+        Box::new(gpioc.pc9.into_pull_down_input(&mut gpioc.crh)),
+        Box::new(gpioc.pc10.into_pull_down_input(&mut gpioc.crh)),
+        Box::new(gpioc.pc11.into_pull_down_input(&mut gpioc.crh)),
+    ];
+    let mut button_driver = ButtonDriver::new(col_pins, row_pins);
+
+    loop {
+        let clicks = button_driver.get_clicks();
+
+        if !clicks.is_empty() {
+            rprintln!("{:?}", clicks);
+        }
+    }
+}
